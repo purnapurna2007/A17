2e7ad64f42d6237a7a383c525055537121df72bd
DEXTER <144078621+purnapurna2007@users.noreply.github.com>
2024-02-22 20:30:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,13 +21,13 @@ global.prefa = [","];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["916297175943", "919362639362"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["916297175943", "919362639362"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["916297175943"];
-global.OwnerName = "Kai";
-global.BotName = "A17";
-global.packname = "A17 Bot";                             //Do not change.
-global.author = "By: Kai";                               //Do not change.
+global.Owner = ["94752058481", ""];         //If you want singal number so global.Owner = ['94752058481'] Change into your number.
+global.OwnerNumber = ["94752058481", ""];   //If you want singal number so global.Owner = ['94752058481'] Change into your number.
+global.ownertag = ["94752058481"];
+global.OwnerName = "Crazy";
+global.BotName = "crazy";
+global.packname = "Crazy";                             //Do not change.
+global.author = "By: Crazy";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai0071/A17"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
 
